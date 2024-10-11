fa81024b51b8cbfb9c6eb570976f3a138aaaa705
Icraku <kutswaivy@gmail.com>
2024-10-12 01:05:28 +0300

    added task 2

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,53 @@
+/* firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
+fullTimeEmployee(boolean) this attribute should always be defined
+yearsOfExperience(number) this attribute is optional
+location(string) this attribute should always be defined
+Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute */
+
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    location: string;
+    yearsOfExperience?: number;
+    [key: string]: any;
+}
+
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+export interface Student_ClassConstructor {
+    new(firstName: string, lastName: string): Student_Class;
+}
+
+export interface Student_Class {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+export class StudentClass implements Student_Class {
+    private firstName: string;
+    private lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
